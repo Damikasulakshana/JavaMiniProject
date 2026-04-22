61c13ad126dc8623958a0bf273b688f543d11471
Bihanpiyumal <bihanpiyumal2023@gmail.com>
2026-04-22 16:11:40 +0530

    Create FinalGradeCalculator.java

---
+++ model/FinalGradeCalculator.java
@@ -0,0 +1,16 @@
+package model;
+import dao.GradeCalculator;
+
+// FinalGradeCalculator – calculates grade from TOTAL marks
+
+public class FinalGradeCalculator extends model.GradeCalculator {
+
+    public FinalGradeCalculator(String courseCode, double totalMarks) {
+        super(courseCode, totalMarks);
+    }
+
+    @Override
+    public String calculate() {
+        return GradeCalculator.calculateGrade(marks);
+    }
+}
