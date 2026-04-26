fe9792114f72fe641931116f7a39ceebedb34f42
Nimesha Samadhi <nimeshadesilva61@gmail.com>
2026-04-26 12:24:03 +0530

    Update Course.java

--- model/Course.java
+++ model/Course.java
@@ -1,16 +1,20 @@
 package model;
-
 public class Course {
 
-    private int    id;
-    private String courseCode;
-    private String courseName;
-    private int    credits;
-    private int    lecturerId;
+    // === VARIABLES ===
+    private int id;              
+    private String courseCode;  
+    private String courseName;  
+    private int credits;        
+    private int lecturerId;     
     private String lecturerName;
 
+    // === CONSTRUCTOR ===
+    // Used to create a Course object with all details
     public Course(int id, String courseCode, String courseName,
                   int credits, int lecturerId, String lecturerName) {
+
+        // Assign values to variables
         this.id           = id;
         this.courseCode   = courseCode;
         this.courseName   = courseName;
@@ -19,16 +23,62 @@ public class Course {
         this.lecturerName = lecturerName;
     }
 
-    public int    getId()           { return id; }
-    public String getCourseCode()   { return courseCode; }
-    public String getCourseName()   { return courseName; }
-    public int    getCredits()      { return credits; }
-    public int    getLecturerId()   { return lecturerId; }
-    public String getLecturerName() { return lecturerName; }
+    // === GETTER METHODS ===
 
-    public void setCourseCode(String v)   { courseCode = v; }
-    public void setCourseName(String v)   { courseName = v; }
-    public void setCredits(int v)         { credits = v; }
-    public void setLecturerId(int v)      { lecturerId = v; }
-    public void setLecturerName(String v) { lecturerName = v; }
+    // Return course ID
+    public int getId() {
+        return id;
+    }
+
+    // Return course code
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    // Return course name
+    public String getCourseName() {
+        return courseName;
+    }
+
+    // Return credit value
+    public int getCredits() {
+        return credits;
+    }
+
+    // Return lecturer ID
+    public int getLecturerId() {
+        return lecturerId;
+    }
+
+    // Return lecturer name
+    public String getLecturerName() {
+        return lecturerName;
+    }
+
+    // === SETTER METHODS ===
+
+    // Update course code
+    public void setCourseCode(String v) {
+        courseCode = v;
+    }
+
+    // Update course name
+    public void setCourseName(String v) {
+        courseName = v;
+    }
+
+    // Update credits
+    public void setCredits(int v) {
+        credits = v;
+    }
+
+    // Update lecturer ID
+    public void setLecturerId(int v) {
+        lecturerId = v;
+    }
+
+    // Update lecturer name
+    public void setLecturerName(String v) {
+        lecturerName = v;
+    }
 }
